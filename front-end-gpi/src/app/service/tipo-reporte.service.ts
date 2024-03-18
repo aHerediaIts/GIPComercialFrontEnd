0c181279db79728500dcd13dcc58e691743afdf1
Sleider Jair Sevillano Ramos <ssevillano@itssolutions.co>
2024-03-18 08:54:20 -0500

    Cambio Reporte Seguimiento

--- front-end-gpi/src/app/service/tipo-reporte.service.ts
+++ front-end-gpi/src/app/service/tipo-reporte.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TipoReporte } from '../model/tipo-reporte';
 import { HttpHeaderApp } from './header';
+import { RecursoActividad } from '../model/recurso-actividad';
 
 @Injectable({
   providedIn: 'root'
@@ -18,4 +19,9 @@ export class TipoReporteService {
     getTiposList(): Observable<TipoReporte[]> {
         return this.httpClient.get<TipoReporte[]>(`${this.baseUrl}/tipos`, { headers: this.header});
     }
+
+    getAllProyectoStatusReport(): Observable<RecursoActividad[]> {
+      return this.httpClient.get<RecursoActividad[]>(`${this.baseUrl}/reporte`, { headers: this.header});
+    }
+    
 }
--- front-end-gpi/src/app/views/pages/reportes/reporte-seguimiento/reporte-seguimiento.component.ts
+++ front-end-gpi/src/app/views/pages/reportes/reporte-seguimiento/reporte-seguimiento.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from '@angular/router';
-import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { TipoReporteService } from "src/app/service/tipo-reporte.service";
 import { TipoReporte } from "src/app/model/tipo-reporte";
+import { RecursoActividad } from "src/app/model/recurso-actividad";
+import * as XLSX from 'xlsx';
 
 @Component({
     selector: 'app-reporte-seguimiento',
@@ -10,31 +11,92 @@ import { TipoReporte } from "src/app/model/tipo-reporte";
     styleUrls: ['./reporte-seguimiento.component.scss']
 })
 export class ReporteSeguimiento implements OnInit{
-    tipoReporteSeleccionado: string = '';
+    tipoReporteSeleccionado: TipoReporte;
     listaTipoReporte: TipoReporte[];
+    listaProyecto: RecursoActividad[];
+
+    constructor(
+        private router: Router,
+        private tipoReporteService: TipoReporteService
+    ){}
 
     ngOnInit(): void {
         this.getTipoReporte();
     }
 
-    constructor( private router: Router,
-        private TipoReporteService: TipoReporteService
-        ){}
-
     getTipoReporte(){
-        this.TipoReporteService.getTiposList().subscribe(data => {
-            data.sort((a, b) => (a.tipo < b.tipo ? -1 : 1));
+        this.tipoReporteService.getTiposList().subscribe(data => {
             this.listaTipoReporte = data;
+            // Inicialmente, seleccionamos el primer tipo de reporte
+            if (this.listaTipoReporte.length > 0) {
+                this.tipoReporteSeleccionado = this.listaTipoReporte[0];
+                this.TipoReporteClick();
+            }
         }, error => {
             console.log(error);
         });
-        console.log(this.listaTipoReporte);
     }
 
-    TipoReporteChange() {
-        console.log('Tipo de Reporte seleccionado:', this.tipoReporteSeleccionado);
-      }
+    TipoReporteClick() {
+        // Dependiendo del tipo de reporte seleccionado, se realiza una acción específica
+        switch(this.tipoReporteSeleccionado.tipo) {
+            case 'INACTIVOS':
+                this.tipoReporteService.getAllProyectoStatusReport().subscribe(data => {
+                    this.listaProyecto = data;
+                });
+                break;
+            case 'CONTROL HORAS':
+            case 'SEGUIMIENTO ANUAL':
+            case 'SEGUIMIENTO INGENIEROS Y ALFA':
+                // Para otros tipos de reporte, inicializamos la listaProyecto como vacía
+                this.listaProyecto = [];
+                break;
+            default:
+                break;
+        }
+    }
+
     hacerReporte()  {
-        console.log('se realizo el reporte')
+        let elementId = '';
+        let sheetName = '';
+
+        // Determinar el ID de la tabla y el nombre de la hoja según el tipo de reporte seleccionado
+        switch(this.tipoReporteSeleccionado.tipo) {
+            case 'INACTIVOS':
+                elementId = 'table-reportes-inactivos';
+                sheetName = 'Reporte de Proyectos Inactivos';
+                break;
+            case 'CONTROL HORAS':
+                elementId = 'table-reportes-control-horas';
+                sheetName = 'Reporte de Control de Horas';
+                break;
+            case 'SEGUIMIENTO ANUAL':
+                elementId = 'table-reportes-seguimiento-anual';
+                sheetName = 'Reporte de Seguimiento Anual';
+                break;
+            case 'SEGUIMIENTO INGENIEROS Y ALFA':
+                elementId = 'table-reportes-seguimiento-ingenieros-alfa';
+                sheetName = 'Reporte de Seguimiento Ingenieros y Alfa';
+                break;
+            default:
+                // En caso de otro tipo de reporte, salir de la función sin hacer nada
+                return;
+        }
+
+        // Obtener el elemento de la tabla según el ID
+        let element = document.getElementById(elementId);
+
+        // Convertir la tabla a una hoja de trabajo de Excel
+        const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+
+        // Definir el nombre del archivo Excel
+        let fileName = this.tipoReporteSeleccionado.tipo + ".xlsx";
+
+        // Agregar la hoja de trabajo al libro de trabajo
+        XLSX.utils.book_append_sheet(wb, ws, sheetName);
+
+        // Descargar el archivo Excel
+        XLSX.writeFile(wb, fileName);
     }
 }
